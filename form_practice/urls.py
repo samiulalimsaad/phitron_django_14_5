c5a300becb5ae394d28705420047c522eb987f8e
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-07 00:43:04 +0600

    route added

--- form_practice/urls.py
+++ form_practice/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from form_practice import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("test/", views.index, name="form_test"),
 ]
