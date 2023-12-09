b3a329e8a7a9b06cbc7f8b0ed2ac140f2a8a9e38
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-09 12:00:30 +0600

    fields added

--- form_practice/form.py
+++ form_practice/form.py
@@ -2,10 +2,27 @@
 # from built-in library
 from django import forms
 
+FAVORITE_COLORS_CHOICES = [
+    ("blue", "Blue"),
+    ("green", "Green"),
+    ("black", "Black"),
+]
+
 
 # creating a form
 class InputForm(forms.Form):
+    roll_number = forms.IntegerField(help_text="Enter 6 digit roll number")
     first_name = forms.CharField(max_length=200)
     last_name = forms.CharField(max_length=200)
-    roll_number = forms.IntegerField(help_text="Enter 6 digit roll number")
+    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
+    address = forms.CharField(widget=forms.Textarea(attrs={"rows": 3}))
+    birth_date = forms.DateField(widget=forms.NumberInput(attrs={"type": "date"}))
+    favorite_color = forms.ChoiceField(choices=FAVORITE_COLORS_CHOICES, required=False)
+    favorite_color_one = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=FAVORITE_COLORS_CHOICES
+    )
+    favorite_colors = forms.MultipleChoiceField(choices=FAVORITE_COLORS_CHOICES)
+    profile_image = forms.FileField()
+
+    agree = forms.BooleanField(label="Agree")
