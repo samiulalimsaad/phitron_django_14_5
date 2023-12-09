97f43e3f4053e23159dab1555b1bb93893f5cdaa
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-09 12:12:55 +0600

    model added

---
+++ form_practice/model.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class TestModal(models.Model):
+    auto_field = models.AutoField(primary_key=True)
+    binary_field = models.BinaryField()
+    boolean_field = models.BooleanField()
+    char_field = models.CharField(max_length=255)
+    date_field = models.DateField()
+    date_time_field = models.DateTimeField()
+    decimal_field = models.DecimalField(max_digits=5, decimal_places=2)
+    duration_field = models.DurationField()
+    email_field = models.EmailField()
+    file_field = models.FileField(upload_to="files/")
+    float_field = models.FloatField()
+    generic_ip_address_field = models.GenericIPAddressField()
+    image_field = models.ImageField(upload_to="images/")
+    integer_field = models.IntegerField()
+    json_field = models.JSONField()
+    null_boolean_field = models.BooleanField(null=True, blank=True)
+    slug_field = models.SlugField()
+    text_field = models.TextField()
+    time_field = models.TimeField()
+    url_field = models.URLField()
+    uuid_field = models.UUIDField()
